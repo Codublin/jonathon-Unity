466158e31f4c6b61c4d41737e64d062876f0189d
Thomasina Egan <eganthomasina@icloud.com>
2014-03-19 15:07:41 +0000

    Brians asssigment - jonathon
    circle moving game lab

---
+++ brian assignment/Assets/CircleFollowing.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleFollowing : MonoBehaviour {
+	
+	private Vector3[] CirclePoints;
+	public float _maxSpeed = 5f;
+	
+	void Start ()
+	{
+		//we assign the circlepoints array 
+		//to the function because the function returns a vector3
+		CirclePoints = CreateCircle(10,10);
+	}
+	
+	void Update ()
+	{
+		DrawCircle();
+		Seek();
+	}
+
+	//this vector3 function returns a vector3. 
+	//The returned vector3 is automatically inserted in
+	//to our CirclePoints array. 
+	Vector3[] CreateCircle(int points, float radius)
+	{
+		Vector3[] CirclePoints = new Vector3[points];
+		//formula for calculating angle of circle
+		float angle = Mathf.PI * 2 / points;
+		
+		for (int i = 0; i < points; i++)
+		{
+			//calculate where this point goes
+			CirclePoints[i] = new Vector3(
+				Mathf.Cos(angle * i), 
+				0,
+				Mathf.Sin(angle * i)
+				) * radius;
+		}
+
+		return CirclePoints;
+
+	}
+	
+	void DrawCircle ()
+	{
+		for(int i = 0; i < CirclePoints.Length; i++)
+		{
+			Debug.DrawLine(CirclePoints[i], CirclePoints[(i + 1) % CirclePoints.Length], Color.red);
+		}
+	}
+
+	void Seek()
+	{
+		//move along the radius of the circle
+		transform.position = new Vector3(Mathf.Cos(Time.time * _maxSpeed), 0, Mathf.Sin(Time.time * _maxSpeed)) * 10;
+	}
+
+}
---
+++ brian assignment/Assets/CubeMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeMove : MonoBehaviour
+{
+	//holds our gameobjects that are used for waypoints
+	public List<Transform> wayPoints = new List<Transform>();
+
+	public float speed = 1.0f;
+	public int index;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//calculates the distance from the current waypoint, and the cube
+		var distance = Vector3.Distance(wayPoints[index].transform.position, transform.position);
+
+		if (distance < 0.5f)
+		{
+			//assign new waypoint only if distance is less than .5
+			index = Random.Range(0, wayPoints.Count);
+		}
+
+		//calculates the current way points position, and my position and subtract 
+		//so i know the distance needed to travel
+		Vector3 direction = wayPoints[index].transform.position - transform.position;
+		direction.Normalize();
+		transform.Translate(direction * Time.deltaTime * speed);
+
+		Debug.DrawLine(transform.position, wayPoints[index].transform.position, Color.red);
+	}
+}
